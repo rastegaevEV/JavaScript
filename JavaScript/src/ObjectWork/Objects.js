1fc9d45032f0cc5f6cd8bec48774f6bb1b38fad6
ASUS <nsk-pto@mail.ru>
2019-07-29 23:24:18 +0700

    obj

---
+++ JavaScript/src/ObjectWork/Objects.js
@@ -0,0 +1,51 @@
+var country1 = {
+    countryName: "Russia",
+    city1: {
+        cityName: "Moscow",
+        population: "12 615 882"
+    },
+    city2: {
+        cityName: "St. Petersburg",
+        population: "5 383 968"
+    },
+    city3: {
+        cityName: "Novosibirsk",
+        population: "1 612 833"
+    },
+    city4: {
+        cityName: "Tomsk",
+        population: "574 002"
+    }
+};
+
+var country2 = {
+    countryName: "Italy",
+    city1: {
+        cityName: "Rome",
+        population: "2 875 805"
+    },
+    city2: {
+        cityName: "Milan",
+        population: "1 351 562"
+    },
+    city3: {
+        cityName: "Naples",
+        population: "970 185"
+    }
+};
+
+var country3 = {
+    countryName: "Germany",
+    city1: {
+        cityName: "Berlin",
+        population: "3 520 031"
+    },
+    city2: {
+        cityName: "Munich",
+        population: "1 450 381"
+    },
+    city3: {
+        cityName: "Hamburg",
+        population: "1 787 408"
+    }
+};
