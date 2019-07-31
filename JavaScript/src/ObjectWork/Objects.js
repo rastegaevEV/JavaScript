bb9254d93563e3dc03b429a1458e38bf1bf691f0
pto <nsk-pto@mail.ru>
2019-07-31 19:16:39 +0700

    JavaScript. Objects

--- JavaScript/src/ObjectWork/Objects.js
+++ JavaScript/src/ObjectWork/Objects.js
@@ -1,51 +1,40 @@
-var country1 = {
-    countryName: "Russia",
-    city1: {
-        cityName: "Moscow",
-        population: "12 615 882"
-    },
-    city2: {
-        cityName: "StPetersburg",
-        population: "5 383 968"
-    },
-    city3: {
-        cityName: "Novosibirsk",
-        population: "1 612 833"
-    },
-    city4: {
-        cityName: "Tomsk",
-        population: "574 002"
-    }
-};
+var countries = [{
+    name: "Russia",
+    cities: [
+        {cityName: "Moscow", population: "12 615 882"},
+        {cityName: "StPetersburg", population: "5 383 968"},
+        {cityName: "Novosibirsk", population: "1 612 833"},
+        {cityName: "Tomsk", population: "574 002"}
+    ]
+}, {
+    name: "Italy",
+    cities: [
+        {cityName: "Rome", population: "2 875 805"},
+        {cityName: "Milan", population: "1 351 562"},
+        {cityName: "Naples", population: "970 185"}
+    ]
+}, {
+    name: "Germany",
+    cities: [
+        {cityName: "Berlin", population: "3 520 031"},
+        {cityName: "Munich", population: "1 450 381"},
+        {cityName: "Hamburg", population: "1 787 408"}
+    ]
+}];
 
-var country2 = {
-    countryName: "Italy",
-    city1: {
-        cityName: "Rome",
-        population: "2 875 805"
-    },
-    city2: {
-        cityName: "Milan",
-        population: "1 351 562"
-    },
-    city3: {
-        cityName: "Naples",
-        population: "970 185"
-    }
-};
+function getMaxCityCountCountry() {
+    var maxCityCount = countries.map(function (value) {
+        return value.cities.length;
+    }).sort(function (a, b) {
+        return b - a;
+    }).shift();
 
-var country3 = {
-    countryName: "Germany",
-    city1: {
-        cityName: "Berlin",
-        population: "3 520 031"
-    },
-    city2: {
-        cityName: "Munich",
-        population: "1 450 381"
-    },
-    city3: {
-        cityName: "Hamburg",
-        population: "1 787 408"
-    }
-};
+    return countries.filter(function (value) {
+        return value.cities.length === maxCityCount
+    }).map(function (value) {
+        return value.name
+    })
+}
+console.log(getMaxCityCountCountry());
+
+
