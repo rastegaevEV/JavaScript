5cad71bef298d940fef28b10e5d0bd9a2236db01
pto <nsk-pto@mail.ru>
2019-08-13 12:16:02 +0700

    TodoList. Страшный, но работает

--- JavaScript/src/TodoList/todoScript.js
+++ JavaScript/src/TodoList/todoScript.js
@@ -8,30 +8,74 @@ document.addEventListener("DOMContentLoaded", function () {
         if (text === "") {
             return;
         }
+        var empty = document.createElement("li");
         var li = document.createElement("li");
         li.textContent = text;
-        todoList.appendChild(li);
+        todoList.appendChild(empty);
+        empty.appendChild(li);
         newText.value = "";
 
         var editText = document.createElement("input");
         editText.type = "text";
         editText.style.display = "none";
-        li.appendChild(editText);
+        empty.appendChild(editText);
 
         var clear = document.createElement("button");
         var lastChild = todoList.lastChild;
 
         clear.textContent = "Удалить";
-        li.appendChild(clear);
+        clear.setAttribute("class", "delete-button");
+        clear.style.marginLeft = "10px";
+        empty.appendChild(clear);
         clear.addEventListener("click", function () {
             todoList.removeChild(lastChild);
         });
 
         var edit = document.createElement("button");
         edit.textContent = "Редактировать";
-        li.appendChild(edit);
-        edit.addEventListener("click", function () {
+        edit.style.marginLeft = "10px";
+        empty.appendChild(edit);
 
+        var cancel = document.createElement("button");
+        cancel.textContent = "Отмена";
+        cancel.style.display = "none";
+        cancel.style.marginLeft = "10px";
+        empty.appendChild(cancel);
+
+        var save = document.createElement("button");
+        save.textContent = "Сохранить";
+        save.style.display = "none";
+        save.style.marginLeft = "10px";
+        empty.appendChild(save);
+        empty.children[0].style.display = "inline-block";
+
+        edit.addEventListener("click", function () {
+            empty.children[1].value = "";
+            empty.children[0].style.display = "none";
+            empty.children[1].style.display = "inline-block";
+            empty.children[2].style.display = "inline-block";
+            empty.children[3].style.display = "none";
+            empty.children[4].style.display = "inline-block";
+            empty.children[5].style.display = "inline-block";
         });
-    })
+
+        save.addEventListener("click", function () {
+            empty.children[0].textContent = empty.children[1].value;
+            empty.children[0].style.display = "inline-block";
+            empty.children[1].style.display = "none";
+            empty.children[2].style.display = "inline-block";
+            empty.children[3].style.display = "inline-block";
+            empty.children[4].style.display = "none";
+            empty.children[5].style.display = "none";
+        });
+
+        cancel.addEventListener("click", function () {
+            empty.children[0].style.display = "inline-block";
+            empty.children[1].style.display = "none";
+            empty.children[2].style.display = "inline-block";
+            empty.children[3].style.display = "inline-block";
+            empty.children[4].style.display = "none";
+            empty.children[5].style.display = "none";
+        });
+    });
 });
